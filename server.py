2bb31fb2f7570ec2a9d3d62d6b01839bf7ceeca0
Jacksonfio <jacksonfio@users.noreply.github.com>
2026-03-29 05:43:26 +0530

    Fix server crash by using simple redirect

--- server.py
+++ server.py
@@ -10,17 +10,11 @@ app = FastAPI(title='Clinical Trial Screener (Old API Docs)')
 
 env = ClinicalTrialEnvironment()
 
-from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.responses import RedirectResponse
 
-@app.get("/", include_in_schema=False)
-async def custom_swagger_ui_html():
-    return get_swagger_ui_html(
-        openapi_url=app.openapi_url,
-        title=app.title + " - API Docs",
-        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-        swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-bundle.js",
-        swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.css",
-    )
+@app.get('/')
+def root():
+    return RedirectResponse(url='/docs')
 
 @app.get('/tasks')
 def list_tasks():
